a1a2ad507f89724868cdc7dbb21bb6152b860e9c
geromt <gerardomartinez205@gmail.com>
2023-01-25 14:47:01 -0600

    feat: add implementation of radixsort algorithm

--- Python/src/radixsort.py
+++ Python/src/radixsort.py
@@ -1,11 +1,43 @@
 
-def sort(array):
+def sort(array: list[int]) -> list[int]:
     buckets = [[] for i in range(10)]
-    for i in array:
-        pass
+    flag_next_iter = False
 
-def _sort_aux(list):
-    pass
+    for i in array:
+        if i < 0:
+            raise ValueError("All the integers must be non-negative numbers")
+        buckets[get_digit(i, 0)].append(i)
+        if i > 9:
+            flag_next_iter = True
+
+    if flag_next_iter:
+        return _sort_aux(buckets, 1)
+    else:
+        return _orginize_buckets(buckets)
+
+
+def _sort_aux(array: list[list[int]], digit: int) -> list[int]:
+    buckets = [[] for i in range(10)]
+    flag_next_iter = False
+    for l in array:
+        for i in l:
+            buckets[get_digit(i, digit)].append(i)
+            if i > 10**(digit+1)-1:
+                flag_next_iter = True
+
+    if flag_next_iter:
+        return _sort_aux(buckets, digit+1)
+    else:
+        return _orginize_buckets(buckets)
+
+
+def _orginize_buckets(buckets: list[list[int]]) -> list[int]:
+    result = []
+    for bucket in buckets:
+        for i in bucket:
+            result.append(i)
+    
+    return result
 
 
 def get_digit(n: int, k: int) -> int:
