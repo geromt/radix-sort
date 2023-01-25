6ce25544a01524689d738cc2cc476008653299c4
geromt <gerardomartinez205@gmail.com>
2023-01-25 11:55:43 -0600

    update: move the files in app/ to src/

--- Python/app/radixsort.py
+++
@@ -1,5 +0,0 @@
-def sort(list):
-    pass
-
-def _sort_aux(list):
-    pass
---
+++ Python/src/radixsort.py
@@ -0,0 +1,11 @@
+
+def sort(array):
+    buckets = [[] for i in range(10)]
+    for i in array:
+        pass
+
+def _sort_aux(list):
+    pass
+
+def get_digit(n, digit):
+    return 0
