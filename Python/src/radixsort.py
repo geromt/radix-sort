f8846dfe8789c444e45bb987f5a3ab5735cfa74b
geromt <gerardomartinez205@gmail.com>
2023-01-25 14:04:03 -0600

    feat: add implementation of get_digit function

--- Python/src/radixsort.py
+++ Python/src/radixsort.py
@@ -7,5 +7,12 @@ def sort(array):
 def _sort_aux(list):
     pass
 
-def get_digit(n, digit):
-    return 0
+
+def get_digit(n: int, k: int) -> int:
+    """Returns the k of n, beginning from the least significant digit, where the least significant 
+    digit is the digit 0, the next of the left is the digit 1, and so on. If k is greater than the
+    number of digits in n, then returns 0"""
+    for i in range(k):
+        n = n // 10
+
+    return n % 10
